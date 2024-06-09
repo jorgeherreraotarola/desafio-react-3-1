3a54a4785ec9fa19924e1cb5aab290853ca8d99b
joejey991 <jidonsop@gmail.com>
2024-06-09 17:42:06 -0400

    Se agrega funcionalidades Listado Formulario y manejar estado App y BaseColaboradores con datos

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
-import { useState } from 'react'
-import './App.css'
+import React, { useState } from 'react';
+import './App.css';
 
-import Buscador from './components/Buscador'
-import Alert from './components/Alert'
-import Formulario from './components/Formulario'
-import Listado from './components/Listado'
+import Buscador from './components/Buscador';
+import Alert from './components/Alert';
+import Formulario from './components/Formulario';
+import Listado from './components/Listado';
+
+import { BaseColaboradores } from './BaseColaboradores';
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
+  const [colaboradores, setColaboradores] = useState(BaseColaboradores);
+
+  const agregarColaborador = (nuevoColaborador) => {
+    setColaboradores([...colaboradores, { ...nuevoColaborador, id: colaboradores.length + 1 }]);
+  };
   const [count, setCount] = useState(0)
 
   return (
@@ -14,8 +22,8 @@ function App() {
         <h1>Lista de colaboradores</h1>
         <Buscador />
         <div className='d-flex flex-column flex-md-row'>
-          <Listado />
-          <Formulario />
+          <Listado colaboradores={colaboradores}/>
+          <Formulario agregarColaborador={agregarColaborador} />
         </div>
       </section>
   )
