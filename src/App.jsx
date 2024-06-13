0d07a0cc90e919503e291f426d330a476f4d6907
Jorge Herrera <jorgeherreraotarola@gmail.com>
2024-06-13 03:07:44 -0400

    Creación de la funcionalidad del buscador

--- src/App.jsx
+++ src/App.jsx
@@ -10,22 +10,29 @@ import { BaseColaboradores } from './BaseColaboradores';
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
-  const [colaboradores, setColaboradores] = useState(BaseColaboradores);
-
+  const [colaboradores, setColaboradores] = useState(BaseColaboradores)
+  const [colaboratorList, setColaboratorList] = useState(BaseColaboradores)
   const agregarColaborador = (nuevoColaborador) => {
-    setColaboradores([...colaboradores, { ...nuevoColaborador, id: colaboradores.length + 1 }]);
+    setColaboradores([...colaboratorList, { ...nuevoColaborador, id: colaboradores.length + 1 }]);
+    setColaboratorList([...colaboratorList, { ...nuevoColaborador, id: colaboradores.length + 1 }]);
   };
   const [count, setCount] = useState(0)
-  const [collaboratorList, setCollaboratorList] = useState(BaseColaboradores)
 
-  const addCollaborator = (e) => {
-    console.log(e)
-    setCollaboratorList([...collaboratorList, e])
+  const filterList = (filterParam) => {
+    if(filterParam == ''){
+      setColaboradores([...colaboratorList])
+    }
+    console.log(filterParam);
+    // arr.filter((el) => el.toLowerCase().includes(query.toLowerCase()));
+    const filterCollaborator = colaboratorList.filter(colaborador => colaborador.nombre.toLowerCase().includes(filterParam.toLowerCase()))
+    console.log(filterCollaborator)
+    setColaboradores([...filterCollaborator])
+    // setCollaboratorList([...collaboratorList, e])
   }
   return (
       <section className='container mt-1'>
         <h1>Lista de colaboradores</h1>
-        <Buscador />
+        <Buscador filterParameter = {filterList}/>
         <div className='d-flex flex-column flex-md-row'>
           <Listado colaboradores={colaboradores}/>
           <Formulario agregarColaborador={agregarColaborador} />
--- src/components/Buscador.jsx
+++ src/components/Buscador.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 
-const Buscador = () => {
+const Buscador = ({filterParameter}) => {
+  const searchCollaborate = (e) => {
+    filterParameter(e.target.value)
+  }
     return (
       <>
         <div className="col col-12">
           <div className="col col-12 col-md-3">
-            <input type="text" className="form-control" placeholder="Buscar un Colaborador"/>
+            <input type="text" className="form-control" placeholder="Buscar un Colaborador" onChange={searchCollaborate}/>
           </div>
         </div>
       </>
